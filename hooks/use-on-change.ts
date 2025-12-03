f5960b9b3098307a321b8499b06379f6eb238c38
ppxb <317842449@qq.com>
2025-12-03 09:09:36 +0800

    chore: add useOnChange hook

---
+++ hooks/use-on-change.ts
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+function isDifferent(a: unknown, b: unknown): boolean {
+	if (Array.isArray(a) && Array.isArray(b)) {
+		return b.length !== a.length || a.some((v, i) => isDifferent(v, b[i]))
+	}
+	return a !== b
+}
+
+/**
+ * @param value - state to watch
+ * @param onChange - when the state changed
+ * @param isUpdated - a function that determines if the state is updated
+ */
+export function useOnChange<T>(
+	value: T,
+	onChange: (cur: T, prev: T) => void,
+	isUpdated: (prev: T, cur: T) => boolean = isDifferent
+) {
+	const [prev, setPrev] = useState<T>(value)
+
+	if (isUpdated(prev, value)) {
+		onChange(value, prev)
+		setPrev(value)
+	}
+}
