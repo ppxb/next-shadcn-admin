58e01c5e78b70c40c60e61be118e1233cca08d3b
ppxb <317842449@qq.com>
2025-12-04 11:39:45 +0800

    chore: add auth layout and login page

---
+++ app/(auth)/login/_components/login-form.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+	Field,
+	FieldDescription,
+	FieldGroup,
+	FieldLabel,
+	FieldSeparator
+} from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
+
+export function LoginForm({
+	className,
+	...props
+}: React.ComponentProps<'div'>) {
+	const [username, setUsername] = useState('john.doe@example.com')
+	const [password, setPassword] = useState('123456')
+	const router = useRouter()
+
+	const handleLogin = (e: React.FormEvent) => {
+		e.preventDefault()
+		router.push('/dashboard')
+	}
+
+	return (
+		<div className={cn('flex flex-col gap-6', className)} {...props}>
+			<Card className="overflow-hidden p-0">
+				<CardContent className="grid p-0 md:grid-cols-2">
+					<form className="p-6 md:p-8" onSubmit={handleLogin}>
+						<FieldGroup>
+							<div className="flex flex-col items-center gap-2 text-center">
+								<h1 className="text-2xl font-bold">Welcome back</h1>
+								<p className="text-muted-foreground text-balance">
+									Login to your account
+								</p>
+							</div>
+							<Field>
+								<FieldLabel htmlFor="email">Email</FieldLabel>
+								<Input
+									id="email"
+									type="email"
+									placeholder="m@example.com"
+									required
+									value={username}
+									onChange={e => setUsername(e.target.value)}
+								/>
+							</Field>
+							<Field>
+								<div className="flex items-center">
+									<FieldLabel htmlFor="password">Password</FieldLabel>
+									<a
+										href="#"
+										className="ml-auto text-sm underline-offset-2 hover:underline"
+									>
+										Forgot your password?
+									</a>
+								</div>
+								<Input
+									id="password"
+									type="password"
+									required
+									value={password}
+									onChange={e => setPassword(e.target.value)}
+								/>
+							</Field>
+							<Field>
+								<Button type="submit" onClick={handleLogin}>
+									Login
+								</Button>
+							</Field>
+							<FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+								OR CONTINUE WITH
+							</FieldSeparator>
+							<Field className="grid grid-cols-3 gap-4">
+								<Button variant="outline" type="button">
+									<svg
+										stroke="currentColor"
+										fill="currentColor"
+										strokeWidth="0"
+										role="img"
+										viewBox="0 0 24 24"
+										className="size-4"
+										height="1em"
+										width="1em"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.141.195v3.325a8.623 8.623 0 0 0-.653-.036 26.805 26.805 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.686 1.686 0 0 0-.679.622c-.258.42-.374.995-.374 1.752v1.297h3.919l-.386 2.103-.287 1.564h-3.246v8.245C19.396 23.238 24 18.179 24 12.044c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.628 3.874 10.35 9.101 11.647Z"></path>
+									</svg>
+								</Button>
+								<Button variant="outline" type="button">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+										<path
+											d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+											fill="currentColor"
+										/>
+									</svg>
+								</Button>
+								<Button variant="outline" type="button">
+									<svg
+										stroke="currentColor"
+										fill="currentColor"
+										strokeWidth="0"
+										role="img"
+										viewBox="0 0 24 24"
+										className="size-4"
+										height="1em"
+										width="1em"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
+									</svg>
+								</Button>
+							</Field>
+							<FieldDescription className="text-center">
+								Don&apos;t have an account? <a href="#">Sign up</a>
+							</FieldDescription>
+						</FieldGroup>
+					</form>
+					<div className="bg-muted relative hidden md:block">
+						<img
+							src="https://shadboard.vercel.app/images/illustrations/misc/welcome.svg"
+							alt="Image"
+							className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+						/>
+					</div>
+				</CardContent>
+			</Card>
+			<FieldDescription className="px-6 text-center">
+				By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
+				and <a href="#">Privacy Policy</a>.
+			</FieldDescription>
+		</div>
+	)
+}
