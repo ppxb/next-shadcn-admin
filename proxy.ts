52b969b6c3235b706e367a8b399a5c301b288b97
ppxb <317842449@qq.com>
2025-12-05 08:54:44 +0800

    refatcor: renaming middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
-export default function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
 	if (request.nextUrl.pathname === '/') {
 		return NextResponse.redirect(new URL('/login', request.url))
 	}
