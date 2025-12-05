96d719b864f6e38f26e5c8536097e8261237a1e7
ppxb <317842449@qq.com>
2025-12-05 16:24:21 +0800

    chore: add recharts

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ChartAreaInteractive } from './_components/chart-area-interactive'
 import { SectionCards } from './_components/section-cards'
 
 export default function Dashboard() {
@@ -8,7 +9,9 @@ export default function Dashboard() {
 			<div className="@container/main flex flex-1 flex-col gap-2">
 				<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 					<SectionCards />
-					<div className="px-4 lg:px-6"></div>
+					<div className="px-4 lg:px-6">
+						<ChartAreaInteractive />
+					</div>
 				</div>
 			</div>
 		</div>
