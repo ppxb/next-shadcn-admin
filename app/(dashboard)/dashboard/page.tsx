4f7e03b6956706b1ec499c94e7d7dc9301840fe8
ppxb <317842449@qq.com>
2026-01-29 16:24:23 +0800

    chore: bump deps

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
 			<div className="@container/main flex flex-1 flex-col gap-2">
 				<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 					<SectionCards />
-					<div className="px-4 lg:px-6">
+					<div className="px-4 lg:px-6 overflow-hidden">
 						<ChartAreaInteractive />
 					</div>
 				</div>
